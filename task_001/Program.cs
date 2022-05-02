6c30f8f2740555074b94e181de85ff2488bee863
igoryan <nkulinigor1984@icloud.com>
2022-05-02 23:37:50 +0200

    task_001

---
+++ task_001/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetNumber(int x)
+{
+
+int a = x / 10;
+int b = x % 10;
+//System.Console.WriteLine($"{value} a: {a} b: {b}");
+int max = a;
+if(b > max) max = b;
+return max;
+}
+int value = new Random().Next(10, 100);
+System.Console.WriteLine(value);
+System.Console.WriteLine(GetNumber(value));
