c6fc400ea3ed67d5cba25a7efeae8a2be286273a
igoryan <nkulinigor1984@icloud.com>
2022-05-04 23:44:54 +0200

    task_007

--- task_007/Program.cs
+++ task_007/Program.cs
@@ -1,3 +1,9 @@
-﻿Console.WriteLine("Введи число -");
-string число = Console.ReadLine();
-Console.WriteLine(число[3]);
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int число = 32679;
+if (число / 100 > 0) Console.WriteLine($"{число / 100 % 10}");
+else Console.WriteLine($"У числа {число} нет третьей цифры");
+
+Console.WriteLine();
+
+
+
