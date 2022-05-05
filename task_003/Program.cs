f1842baee12ffc5f83c1d1c3d3fa5de1b476a86a
igoryan <nkulinigor1984@icloud.com>
2022-05-05 21:19:45 +0200

    task_003

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+int Number1 = 16;
+int Number2 = 4;
+int result = 0;
+
+int MultipleNumber()
+{
+    result = Number1 % Number2;
+    return result;
+}
+MultipleNumber();
+Console.WriteLine();
+
+if (result == 0) Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+else
+{
+    Console.WriteLine($"{Number1} не кратно {Number2}, остаток от деления равен {Number1 % Number2}");
+}
+Console.WriteLine();
